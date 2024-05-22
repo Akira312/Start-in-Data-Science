9319498f807781ca1d04dd6f73ede849f8f0d912
Akira <databaseanalyser@gmail.com>
2024-05-22 23:30:41 +0700

    stabilize decorator

--- ObjectOrientedProgramming.py
+++ ObjectOrientedProgramming.py
@@ -1,13 +1,22 @@
 import time as t
 def benchmark(func):
-    start=t.time()
-    func()
-    stop=t.time()
-    print(stop-start)
+    def time():
+        start=t.time()
+        print(func())
+        stop=t.time()
+        print(stop-start)
+    return time
 
+def logging(func):
+    def log():
+        pass
+    return log
+
+@logging
 @benchmark
 def function():
     s=0
-    for i in range(10000000):
+    for i in range(9999999):
         s+=i
     return s
+function()

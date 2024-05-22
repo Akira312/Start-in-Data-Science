ac66c848f5ccd77c9a4cd2046855b014c27421f1
Akira <databaseanalyser@gmail.com>
2024-05-22 22:48:20 +0700

    Add function and decorator for it

--- ObjectOrientedProgramming.py
+++ ObjectOrientedProgramming.py
@@ -1,1 +1,13 @@
+import time as t
+def benchmark(func):
+    start=t.time()
+    func()
+    stop=t.time()
+    print(stop-start)
 
+@benchmark
+def function():
+    s=0
+    for i in range(10000000):
+        s+=i
+    return s
